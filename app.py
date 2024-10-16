1f4a843d206f4a02afadf6f72f27d7cfdb364aca
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-16 18:22:27 +0300

    Registration page

--- app.py
+++ app.py
@@ -1,3 +1,65 @@
+def registration_page():
+    """
+    User registration page for their details.
+    Redirect users to main page after registration.
+    """
+    # Registration banner
+    registration_banner = """
++++++==============================+++++
++          REGISTRATION PAGE           +
++--------------------------------------+
++Don't have an account? Create one here+
++++++=========================++++++++++
+
+Fill in the necessary details below:
+"""
+    print(registration_banner)
+
+    # Prompting the manager for details
+    manager_id = int(input("Enter your manager ID: "))
+    manager_name = input("Enter your full name: ")
+    years_of_experience = int(input("Enter your years of experience: "))
+    email_address = input("Enter your email address: ")
+    password = input("Enter your password: ")
+    phone_number = int(input("Enter your phone number: "))
+
+    # Welcoming new user
+    print(f"TRegistration successful. Welcome {manager_name} to Athloverse.")
+
+    # Redirect to main page
+    print("Redirecting to main page...")
+    main()
+
+
+def main_menu():
+    """
+    Displays the main menu with user options such as register,
+    login and exiting the program.
+    """
+
+    # Menu containing user options
+    menu = """
+Select from the main menu:
+1. Register
+2. Log in
+3. Exit
+    """
+    print(menu)
+    # Prompt user for input
+    user_choice = int(input())
+
+    # Validate user choice
+    if user_choice == 1:
+        registration_page()
+    elif user_choice == 2:
+        pass
+    elif user_choice == 3:
+        pass
+    else:
+        print("Invalid choice. Please enter a correct choice.")
+        main()
+
+
 def main():
     """
     Displays main welcome page
@@ -10,27 +72,10 @@ def main():
 +--------------------------------------+
 +    CLOTHES, EQUIPMENT, NAME IT...    +
 +++++==============================+++++
-
-Select from the main menu:
-1. Register
-2. Log in
-3. Exit
 """
 
     print(welcome_banner)
-    # Prompt user for input
-    user_choice = int(input())
-
-    # Validate user choice
-    if user_choice == 1:
-        pass
-    elif user_choice == 2:
-        pass
-    elif user_choice == 3:
-        pass
-    else:
-        print("Invalid choice. Please enter a correct choice.")
-        main()
+    main_menu()
 
 
 # Calling main() function
